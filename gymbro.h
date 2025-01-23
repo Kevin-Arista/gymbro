59abae66ef40cbf6373324c2cc79528e2059b0ca
Kevin Arista S <kevin.a8988@gmail.com>
2025-01-23 17:41:24 -0500

    fixed definition of global variables

--- gymbro.h
+++ gymbro.h
@@ -4,11 +4,7 @@
 // "array of pointers that point to characters"
 const char *keys[] = {"weight", "food", "journal", "exercise"};
 
-char flags[5];
-flags[0] = 'W';
-flags[1] = 'F';
-flags[2] = 'J';
-flags[3] = 'E';
-flags[4] = '\0';
+// an array of characters
+char flags[] = "WFJE";
 
 #endif
