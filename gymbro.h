eb6c6f8fe03eccea14f62d1f6e3367a5e3e350b1
Kevin Arista S <kevin.a8988@gmail.com>
2025-01-25 21:41:58 -0500

    redefined keys and flag array of strings in order to facilitate string comparison

--- gymbro.h
+++ gymbro.h
@@ -2,9 +2,14 @@
 #define GYMBRO_H
 
 // "array of pointers that point to characters"
-const char *keys[] = {"weight", "food", "journal", "exercise"};
+const char *keys[] = {"--weight", "--food", "--journal", "--exercise"};
 
-// an array of characters
-char flags[] = "WFJE";
+// an array of pointers to character value (may end until we reach null terminator)
+const char *flags[] = {"-W", "-F", "-J", "-E"};
+
+typedef struct {
+	const char *key;
+	const char *flagv[];
+} action;
 
 #endif
