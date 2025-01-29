f11c6a8de374f334be0688ccf8758426d6b4fdf6
Kevin Arista S <kevin.a8988@gmail.com>
2025-01-29 00:32:00 -0500

    designed first iteration of meal/food struct

--- gymbro.h
+++ gymbro.h
@@ -8,8 +8,31 @@ const char *keys[] = {"--weight", "--food", "--journal", "--exercise"};
 const char *flags[] = {"-W", "-F", "-J", "-E"};
 
 typedef struct {
-	const char *key;
-	const char *flagv[];
+        const char *key;
+        const char *flagv[];
 } action;
 
 #endif
+
+typedef struct {
+        char name[25]; // anything
+        char type[10]; // breakfast, lunch, dinner, snack
+        char processed; // yes(y) or no(n);
+        float grams; // in grams
+
+        int calories; // auto                                 
+        int year; // auto
+        int month; // auto
+        int day; // auto
+        int hour; // auto
+        int minute; // auto
+} food;
+
+typedef struct {
+        int weight;
+        meal food;
+} workout;
+
+typedef struct {
+
+} log;
